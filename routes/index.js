5b53f1bf163d419df1e3eef2f4108194e4210396
Paublo Martinez <pamg26894@gmail.com>
2019-05-11 01:15:00 -0600

    true y false...

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,18 @@ const express = require('express');
 
 /*************Instancias controladores administrador****************/
 const user = require('../controllers/users/bodega');
+const auth = require('../controllers/mdl');
 /*******************************************************************/
 
 const api = express.Router();
 
-api.get('/Bodega/obtenerInventario',user.obtenerInventario);
-
+api.get('/Bodega/obtenerInventario',auth.checkToken,user.obtenerInventario);
+api.post('/Bodega/realizarDespacho',auth.checkToken,user.despacharOrden);
 
 api.get('/Bodega/getInventarioReal',user.getInventarioReal);
 api.get('/Bodega/getTiempoEntrega',user.getTiempoEntrega);
 api.get('/Bodega/suscripcionTienda',user.suscripcionTienda);
-api.post('/Bodega/realizarDespacho',user.despacharOrden);
+
 api.get('/Bodega/consultarPIM/',user.catalogoPIM);
 
 module.exports = api;
